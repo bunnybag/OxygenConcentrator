aac70da2cb5c1191a3191d7d5c7b8d6b2d4f4038
Suresh Kumar <SureshKarthik@users.noreply.github.com>
2021-05-27 09:46:58 +0530

    Updated LCD format and ref mv

--- oxygenconcentrator.ino
+++ oxygenconcentrator.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 
-#define backgroundReading 150 // mV
+#define backgroundReading 146 // mV
+#define LCD
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 unsigned long time_ms;
@@ -13,17 +14,23 @@ int sensorValue;
 float sensorVoltage;
 float adcResolution;
 float O2_level;
+char O2_level_str[6];
 
 void setup() {
+  #ifdef LCD
   lcd.init();
   lcd.clear();
   lcd.backlight();
   lcd.home();
+  #endif
   
   Serial.begin(9600);
   Serial.println("O2 Concentrator");
+
+  #ifdef LCD
   lcd.setCursor(0,0);
   lcd.print("O2 Concentrator");
+  #endif
 
   scaleFactor = (float) backgroundReading / 21.0;
   adcResolution = (float) (5000.0 / 1023.0);
@@ -35,9 +42,11 @@ void loop() {
   time_s = time_ms/1000%60;
   time_min = time_ms/1000/60%60;
   time_hr = time_ms/1000/60/60;
-  sprintf(time_string, "%02d:%02d:%02d", time_hr, time_min, time_s);
+  sprintf(time_string, "%02d:%02d", time_hr, time_min);
+  #ifdef LCD
   lcd.setCursor(0,1);
   lcd.print(time_string);
+  #endif
 
   sensorValue = analogRead(A0);
   sensorVoltage = (float) sensorValue * adcResolution;
@@ -50,7 +59,15 @@ void loop() {
   Serial.print(sensorVoltage);
   Serial.print("  ");
   Serial.println(O2_level);
-  lcd.setCursor(10,1);
+  #ifdef LCD
+  lcd.setCursor(6,1);
+  //sprintf(O2_level_str, "%3d %f %%",int(sensorVoltage),O2_level);
+  lcd.print(int(sensorVoltage));
+  lcd.print(" ");
   lcd.print(String(O2_level,2));
   lcd.print("%");
+  lcd.print(" ");
+  lcd.print(" ");
+  lcd.print(" ");
+  #endif
 }
